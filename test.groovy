753ef7c8eedd38caa4e45821bb756bf133fac056
kvishnuv <koyyada.vishnuvardhan@nokia.com>
2023-03-30 12:31:52 +0530

    adding test file

--- test.groovy
+++ test.groovy
@@ -1,15 +1,1 @@
-pipelineJob('job-dsl-plugin') {
-  definition {
-    cpsScm{
-      scm {
-        git {
-          remote {
-            url('https://github.com/vishnukoyyada/my-codes')
-          }
-          branch('main')
-        }
-      }
-      lightweight()
-    }
-  }
-}
+hello
