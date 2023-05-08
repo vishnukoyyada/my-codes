2ae1a2b4c212fec794bfe35cf268f43e089fea03
kvishnuv <koyyada.vishnuvardhan@nokia.com>
2023-05-08 18:03:13 +0530

    updated  test.grrovy for jira purpose 3

--- test.groovy
+++ test.groovy
@@ -1,2 +1,4 @@
 BUILD_TRIGGER_BY = currentBuild.getBuildCauses()[0].shortDescription + " / " + currentBuild.getBuildCauses()[0].userId
-echo "${BUILD_TRIGGER_BY}"
+if (BUILD_TRIGGER_BY == "kvishnuv"){
+    print("yess")
+}
