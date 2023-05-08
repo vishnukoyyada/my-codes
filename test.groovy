748ade6ddf5c8c7053af2e5e4d51751318fbd199
kvishnuv <koyyada.vishnuvardhan@nokia.com>
2023-05-08 18:00:23 +0530

    updated  test.grrovy for jira purpose 2

--- test.groovy
+++ test.groovy
@@ -1,2 +1,2 @@
 BUILD_TRIGGER_BY = currentBuild.getBuildCauses()[0].shortDescription + " / " + currentBuild.getBuildCauses()[0].userId
-echo "BUILD_TRIGGER_BY: ${BUILD_TRIGGER_BY}"
+echo "${BUILD_TRIGGER_BY}"
