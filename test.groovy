62bf229a84a05e96c21b5e1eec5dce6f64119d74
kvishnuv <koyyada.vishnuvardhan@nokia.com>
2023-05-09 11:29:09 +0530

    updating the test.grrovy file for the fifth time.

--- test.groovy
+++ test.groovy
@@ -1,3 +1,5 @@
-def BUILD_TRIGGER_BY =currentBuild.getBuildCauses()[0].userId
-echo"$BUILD_TRIGGER_BY"
-println "$BUILD_TRIGGER_BY.getClass()"
+BUILD_TRIGGER_BY =currentBuild.getBuildCauses()[0].userId
+
+if(BUILD_TRIGGER_BY == "null"){
+    println("yess");
+}
