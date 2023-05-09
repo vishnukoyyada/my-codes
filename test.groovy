248c310fa2b3ba864da54855325423e41539cfac
kvishnuv <koyyada.vishnuvardhan@nokia.com>
2023-05-09 09:12:31 +0530

    updated  test.grrovy for jira purpose 6

--- test.groovy
+++ test.groovy
@@ -1,3 +1,3 @@
-BUILD_TRIGGER_BY =currentBuild.getBuildCauses()[0].userId
+def BUILD_TRIGGER_BY =currentBuild.getBuildCauses()[0].userId
 echo"$BUILD_TRIGGER_BY"
 println "$BUILD_TRIGGER_BY.getClass()"
