4360e344907fbad6106f33b772522d36469acff2
kvishnuv <koyyada.vishnuvardhan@nokia.com>
2023-05-25 11:23:49 +0530

    testing

--- test.groovy
+++ test.groovy
@@ -1,50 +1,17 @@
 pipeline {
     agent any
-
-    job('ExampleJob') {
+    
     stages {
-        stage('Mandatory and Non-Mandatory Stage') {
+        stage('Print Hello World') {
             steps {
                 script {
-                    try {
-                        stage('Mandatory Stage') {
-                            // Steps of the mandatory stage
-                            // ...
-                  
-                            println("mandtory stage has failed");
-                            if (stage('Mandatory Stage').result != 'SUCCESS') {
-                                throw new Exception("Mandatory stage failed.")
-                            }
-                        }
-                        
-                        stage('Non-Mandatory Stage') {
-                            // Steps of the non-mandatory stage
-                            // ...
-                            if (satge('Non-Mandatory Stage').result != 'SUCCESS') {
-                            println("Non-mandatory stage failed.");
-                                // You can perform additional actions if needed
-                            }
-                        }
-                    } catch (Exception e) {
-                        // Handle the exception as needed
-                        if (stage('Mandatory Stage').result != 'SUCCESS') {
-                            // Print a specific message when the mandatory stage fails
-                            echo "Mandatory stage failed: ${e.message}"
-                        } else {
-                            // Print the failure message for non-mandatory stage
-                            echo "Non-mandatory stage failed: ${e.message}"
-                        }
-                    }
+                    echo "Hello, World!"
                 }
             }
         }
         
-        // Other stages...
     }
     
-    // Other job configuration...
+    // Additional pipeline configuration
+    // ...
 }
-
-}
-
-
